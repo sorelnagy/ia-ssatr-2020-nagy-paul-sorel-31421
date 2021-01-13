0b905db81e114fc54bb08f059f434d12f6e3c8b4
Nagy Paul Sorel <nagyp.sorel@gmail.com>
2021-01-13 19:18:14 +0200

    Laborator 1

---
+++ laborator1/src/main/java/com/nps/laborator1/Lab1Ex0.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nps.laborator1;
+
+/**
+ *
+ * @author nagyp
+ */
+public class Lab1Ex0 {
+    public static void main(String[] args) {
+        System.out.println("Laborator1");
+    }
+}
